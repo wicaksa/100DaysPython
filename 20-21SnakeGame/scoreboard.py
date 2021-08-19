cf8e2c7c28a6ecc2f061c0087361cfec4dba9ae3
wicaksa <50036161+wicaksa@users.noreply.github.com>
2021-08-19 12:33:09 -0700

    Create scoreboard.py

---
+++ 20-21SnakeGame/scoreboard.py
@@ -0,0 +1,42 @@
+# This class keeps track of the score of the game.
+from turtle import Turtle
+
+# Constants
+X_COR = 0
+Y_COR = 300
+FONT = ("Courier", 24, "normal")
+ALIGNMENT = "Center"
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.goto(X_COR, Y_COR)
+        self.pendown()
+        self.color("white")
+        self.score = 0
+        self.writeScore()
+        self.hideturtle()
+
+    # Updates the score in the game.
+    def updateScore(self):
+        self.score += 1
+        self.clear()
+        self.writeScore()
+
+    # Writes the Score on the screen.
+    def writeScore(self):
+        self.write(arg=f"Score : {self.score}",
+                   move=False,
+                   align=ALIGNMENT,
+                   font=FONT)
+
+    # Writes the Game over Screen.
+    def gameOver(self):
+        self.goto(0, 0)
+        self.pendown()
+        self.color("white")
+        self.write(arg="Game Over",
+                   move=False,
+                   align=ALIGNMENT,
+                   font=FONT)
